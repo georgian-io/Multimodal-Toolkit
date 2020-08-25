0784d0b712c7db5eb5f649516b01f63aa487cb52
kengu <ken.qgu@gmail.com>
2020-08-24 21:01:40 -0700

    working bert, roberta, distillbert, and automodel

--- multimodal_transformers.py
+++ multimodal_transformers.py
@@ -1,6 +1,9 @@
 from torch import nn
-from torch.nn import CrossEntropyLoss, MSELoss
-from transformers import BertForSequenceClassification, BertModel, BertPreTrainedModel
+from transformers import (
+    BertForSequenceClassification,
+    RobertaForSequenceClassification,
+    DistilBertForSequenceClassification,
+)
 
 from combine_tabular_feat import TabularFeatCombiner
 from layer_utils import MLP, calc_mlp_dims, hf_loss_func
@@ -12,18 +15,18 @@ class BertWithTabular(BertForSequenceClassification):
         super().__init__(hf_model_config)
         self.tabular_combiner = TabularFeatCombiner(hf_model_config)
         combined_feat_dim = self.tabular_combiner.final_out_dim
-        tabular_config = hf_model_config.tabular_config
-        if tabular_config.use_simple_classifier:
+        self.tabular_config = hf_model_config.tabular_config
+        if self.tabular_config.use_simple_classifier:
             self.tabular_classifier = nn.Linear(combined_feat_dim,
-                                                hf_model_config.num_labels)
+                                                self.tabular_config.num_labels)
         else:
             dims = calc_mlp_dims(combined_feat_dim,
-                                 division=tabular_config.mlp_division,
-                                 output_dim=tabular_config.num_labels)
+                                 division=self.tabular_config.mlp_division,
+                                 output_dim=self.tabular_config.num_labels)
             self.tabular_classifier = MLP(combined_feat_dim,
-                                          tabular_config.num_labels,
+                                          self.tabular_config.num_labels,
                                           num_hidden_lyr=len(dims),
-                                          dropout_prob=tabular_config.mlp_dropout,
+                                          dropout_prob=self.tabular_config.mlp_dropout,
                                           hidden_channels=dims,
                                           bn=True)
 
@@ -37,6 +40,8 @@ class BertWithTabular(BertForSequenceClassification):
         inputs_embeds=None,
         labels=None,
         class_weights=None,
+        output_attentions=None,
+        output_hidden_states=None,
         cat_feats=None,
         numerical_feats=None
     ):
@@ -48,6 +53,8 @@ class BertWithTabular(BertForSequenceClassification):
             position_ids=position_ids,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
         )
         pooled_output = outputs[1]
         pooled_output = self.dropout(pooled_output)
@@ -57,8 +64,128 @@ class BertWithTabular(BertForSequenceClassification):
         loss, logits, classifier_layer_outputs = hf_loss_func(combined_feats,
                                                               self.tabular_classifier,
                                                               labels,
-                                                              self.num_labels,
+                                                              self.tabular_config.num_labels,
                                                               class_weights)
         return loss, logits, classifier_layer_outputs
 
 
+class RobertaWithTabular(RobertaForSequenceClassification):
+
+    def __init__(self, hf_model_config):
+        super().__init__(hf_model_config)
+        self.tabular_combiner = TabularFeatCombiner(hf_model_config)
+        combined_feat_dim = self.tabular_combiner.final_out_dim
+        self.tabular_config = hf_model_config.tabular_config
+        self.dropout = nn.Dropout(hf_model_config.hidden_dropout_prob)
+        if self.tabular_config.use_simple_classifier:
+            self.tabular_classifier = nn.Linear(combined_feat_dim,
+                                                self.tabular_config.num_labels)
+        else:
+            dims = calc_mlp_dims(combined_feat_dim,
+                                 division=self.tabular_config.mlp_division,
+                                 output_dim=self.tabular_config.num_labels)
+            self.tabular_classifier = MLP(combined_feat_dim,
+                                          self.tabular_config.num_labels,
+                                          num_hidden_lyr=len(dims),
+                                          dropout_prob=self.tabular_config.mlp_dropout,
+                                          hidden_channels=dims,
+                                          bn=True)
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        class_weights=None,
+        cat_feats=None,
+        numerical_feats=None
+    ):
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+
+        sequence_output = outputs[0]
+        text_feats = sequence_output[:, 0, :]
+        text_feats = self.dropout(text_feats)
+        combined_feats = self.tabular_combiner(text_feats,
+                                               cat_feats,
+                                               numerical_feats)
+        loss, logits, classifier_layer_outputs = hf_loss_func(combined_feats,
+                                                              self.tabular_classifier,
+                                                              labels,
+                                                              self.tabular_config.num_labels,
+                                                              class_weights)
+        return loss, logits, classifier_layer_outputs
+
+
+class DistilBertWithTabular(DistilBertForSequenceClassification):
+    def __init__(self, hf_model_config):
+        super().__init__(hf_model_config)
+        self.tabular_combiner = TabularFeatCombiner(hf_model_config)
+        combined_feat_dim = self.tabular_combiner.final_out_dim
+        self.tabular_config = hf_model_config.tabular_config
+        self.dropout = nn.Dropout(hf_model_config.seq_classif_dropout)
+        if self.tabular_config.use_simple_classifier:
+            self.tabular_classifier = nn.Linear(combined_feat_dim,
+                                                self.tabular_config.num_labels)
+        else:
+            dims = calc_mlp_dims(combined_feat_dim,
+                                 division=self.tabular_config.mlp_division,
+                                 output_dim=self.tabular_config.num_labels)
+            self.tabular_classifier = MLP(combined_feat_dim,
+                                          self.tabular_config.num_labels,
+                                          num_hidden_lyr=len(dims),
+                                          dropout_prob=self.tabular_config.mlp_dropout,
+                                          hidden_channels=dims,
+                                          bn=True)
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        class_weights=None,
+        cat_feats=None,
+        numerical_feats=None
+    ):
+
+        distilbert_output = self.distilbert(
+            input_ids,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+        hidden_state = distilbert_output[0]  # (bs, seq_len, dim)
+        pooled_output = hidden_state[:, 0]  # (bs, dim)
+        pooled_output = self.pre_classifier(pooled_output)  # (bs, dim)
+        pooled_output = nn.ReLU()(pooled_output)  # (bs, dim)
+        text_feats = self.dropout(pooled_output)
+        combined_feats = self.tabular_combiner(text_feats,
+                                               cat_feats,
+                                               numerical_feats)
+        loss, logits, classifier_layer_outputs = hf_loss_func(combined_feats,
+                                                              self.tabular_classifier,
+                                                              labels,
+                                                              self.tabular_config.num_labels,
+                                                              class_weights)
+        return loss, logits, classifier_layer_outputs
--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,16 @@ from os.path import dirname, abspath, join
 import sys
 
 import torch
-from transformers import BertConfig, BertTokenizer, AdamW
+from transformers.configuration_auto import (
+    BertConfig, RobertaConfig, DistilBertConfig,
+)
+from transformers import (
+    BertTokenizer,
+    RobertaTokenizer,
+    AutoTokenizer,
+    DistilBertTokenizer,
+    AdamW
+)
 
 # make sure can import modules from the current directory
 cur_folder = dirname(abspath(''))
@@ -11,33 +20,69 @@ sys.path.insert(0, cur_folder)
 sys.path.insert(1, src_folder)
 
 from multimodal_config import TabularConfig
-from multimodal_transformers import BertWithTabular
+from multimodal_transformers import (
+    BertWithTabular, RobertaWithTabular, DistilBertWithTabular
+)
+from multimodal_modeling_auto import AutoModelWithTabular
+
+MODEL_CLASSES = {
+    'bert_w_tabular': (BertConfig, BertWithTabular, BertTokenizer, 'bert-base-uncased'),
+    'roberta_w_tabular': (RobertaConfig, RobertaWithTabular, RobertaTokenizer, 'roberta-base'),
+    'finbert_w_tabular': (BertConfig, AutoModelWithTabular, AutoTokenizer, 'ipuneetrathore/bert-base-cased-finetuned-finBERT'),
+    'distilbert_w_tabular': (DistilBertConfig, DistilBertWithTabular, DistilBertTokenizer, 'distilbert-base-uncased')
+}
+
+
+model = 'distilbert_w_tabular'  # change this to try different with tabular models
 
 # Load pre-trained model tokenizer (vocabulary)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = MODEL_CLASSES[model][2].from_pretrained(MODEL_CLASSES[model][3])
 
 # Tokenize input
-text1 = '[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]'
-text2 = '[CLS] Just some random text [SEP]'
-tokenized_text1 = tokenizer.tokenize(text1)
-tokenized_text2 = tokenizer.tokenize(text2)
+if model in ['bert_w_tabular', 'finbert_w_tabular', 'distilbert_w_tabular']:
+    text1 = 'Who was Jim Henson ?', 'Jim Henson was a puppeteer'
+    text2 = 'Just some random text'
 
-# Convert token to vocabulary indices
-indexed_tokens1 = tokenizer.convert_tokens_to_ids(tokenized_text1)
-indexed_tokens2 = tokenizer.convert_tokens_to_ids(tokenized_text2)
-#  Need to do padding for the shorter sentence
-indexed_tokens2 = indexed_tokens2 + [0] * (len(indexed_tokens1) - len(indexed_tokens2))
-# Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
-segments_ids1 = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
-segments_ids2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tokenized_text1 = tokenizer.tokenize(text1[0]), tokenizer.tokenize(text1[1])
+    tokenized_text2 = tokenizer.tokenize(text2)
+    # Convert token to vocabulary indices
+    indexed_tokens1 = (tokenizer.convert_tokens_to_ids(tokenized_text1[0]),
+                       tokenizer.convert_tokens_to_ids(tokenized_text1[1]))
+    indexed_tokens2 = tokenizer.convert_tokens_to_ids(tokenized_text2)
 
-# Convert inputs to PyTorch tensors
-tokens_tensor = torch.tensor([indexed_tokens1, indexed_tokens2])
-segments_tensors = torch.tensor([segments_ids1, segments_ids2])
+    # Add special tokens
+    tokenized_ids1 = tokenizer.build_inputs_with_special_tokens(indexed_tokens1[0],
+                                                                indexed_tokens1[1])
+    tokenized_ids2 = tokenizer.build_inputs_with_special_tokens(indexed_tokens2)
+
+    # Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
+    segments_ids1 = tokenizer.create_token_type_ids_from_sequences(*indexed_tokens1)
+    segments_ids2 = tokenizer.create_token_type_ids_from_sequences(indexed_tokens2)
+
+    #  Need to do padding for the shorter sentence
+    tokenized_ids2 = tokenized_ids2 + [0] * (len(tokenized_ids1) - len(tokenized_ids2))
+    segments_ids2 = segments_ids2 + [0] * (len(segments_ids1) - len(segments_ids2))
+
+    # Convert inputs to PyTorch tensors
+    tokens_tensor = torch.tensor([tokenized_ids1, tokenized_ids2])
+    if model in ['bert_w_tabular', 'finbert_w_tabular']:
+        segments_tensors = torch.tensor([segments_ids1, segments_ids2])
+    else:
+        segments_tensors = None
+
+elif model == 'roberta_w_tabular':
+    text1 = 'Who was Jim Hensen ? </s> Jim Henson was a puppeteer'
+    text2 = 'Just some random text'
+    indexed_tokens1 = tokenizer(text1)['input_ids']
+    indexed_tokens2 = tokenizer(text2)['input_ids']
+    indexed_tokens2 = indexed_tokens2 + [0] * (len(indexed_tokens1) - len(indexed_tokens2))
+    tokens_tensor = torch.tensor([indexed_tokens1, indexed_tokens2])
+    segments_tensors = None
+
+print(f'input ids shape: {tokens_tensor.shape}')
 
 # 5 numerical features
 numerical_feat = torch.rand(2, 5).float()
-print(numerical_feat.shape)
 # 9 categorical features
 categorical_feat = torch.tensor([[0, 0, 0, 1, 0, 1, 0, 1, 0],
                                  [1, 0, 0, 0, 1, 0, 1, 0, 0]]).float()
@@ -46,25 +91,26 @@ labels = torch.tensor([1, 0])
 
 # See BasicBertConfig for more details on configuring Bert
 tabular_config = TabularConfig(
-    combine_feat_method='gating_on_cat_and_num_feats_then_sum',  # change this to specify the method of combining tabular data
+    combine_feat_method='attention_on_cat_and_numerical_feats',  # change this to specify the method of combining tabular data
     cat_feat_dim=9,  # need to specify this
     numerical_feat_dim=5,  # need to specify this
     num_labels=2,   # need to specify this
     use_num_bn=False,  # No bn since we only have one training example for demonstration purposes
 )
 
-bert_config = BertConfig()
-bert_config.tabular_config = tabular_config
+hf_config = MODEL_CLASSES[model][0].from_pretrained(MODEL_CLASSES[model][3])
+hf_config.tabular_config = tabular_config
+
 
 device = str('cuda' if torch.cuda.is_available() else 'cpu')
 
 # loads the pre-trained weights for Bert module
-model = BertWithTabular.from_pretrained('bert-base-uncased', config=bert_config)
+model = MODEL_CLASSES[model][1].from_pretrained(MODEL_CLASSES[model][3], config=hf_config)
 print(model)
 
 
 tokens_tensors = tokens_tensor.to(device)
-segments_tensors = segments_tensors.to(device)
+segments_tensors = segments_tensors.to(device) if segments_tensors is not None else None
 numerical_feat = numerical_feat.to(device)
 categorical_feat = categorical_feat.to(device)
 labels = labels.to(device)
@@ -74,9 +120,15 @@ model.to(device)
 optimizer = AdamW(model.parameters(), lr=5e-5)
 # One forward pass and one backward pass
 model.train()
-loss, _, _ = model(tokens_tensors, token_type_ids=segments_tensors,
-                   labels=labels, cat_feats=categorical_feat,
-                   numerical_feats=numerical_feat)
+if segments_tensors is not None:
+    loss, _, _ = model(tokens_tensors, token_type_ids=segments_tensors,
+                       labels=labels, cat_feats=categorical_feat,
+                       numerical_feats=numerical_feat)
+else:
+    loss, _, _ = model(tokens_tensors,
+                       labels=labels, cat_feats=categorical_feat,
+                       numerical_feats=numerical_feat)
+
 print(f'Loss: {loss.item()}')
 loss.backward()
 optimizer.step()
@@ -86,11 +138,17 @@ model.zero_grad()
 model.eval()
 with torch.no_grad():
     # If labels are passed then loss will also be returned
-    _, logits, classifier_outputs = model(tokens_tensors,
-                                          token_type_ids=segments_tensors,
-                                          cat_feats=categorical_feat,
-                                          numerical_feats=numerical_feat
-                                          )
+    if segments_tensors is not None:
+        _, logits, classifier_outputs = model(tokens_tensors,
+                                              token_type_ids=segments_tensors,
+                                              cat_feats=categorical_feat,
+                                              numerical_feats=numerical_feat
+                                              )
+    else:
+        _, logits, classifier_outputs = model(tokens_tensors,
+                                              cat_feats=categorical_feat,
+                                              numerical_feats=numerical_feat
+                                              )
     print(f'Logits:\n{logits.detach().numpy()}')  # shape: batch_size x num_labels
     # The classifier_output is specified by last_ith_layer_as_embd of the bert config
     # The input, hidden, and output embeddings of the final classifier are saved as a list of tensors
