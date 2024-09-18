3a036fd9201afb72084649f02688d9e7cb22fc51
Akash Saravanan <akash.saravanan@georgian.io>
2024-09-18 11:27:55 -0600

    docs: type hints & docstrings

--- multimodal_transformers/data/tabular_torch_dataset.py
+++ multimodal_transformers/data/tabular_torch_dataset.py
@@ -1,5 +1,9 @@
+from typing import List, Optional, Union
+
 import numpy as np
+import pandas as pd
 import torch
+import transformers
 from torch.utils.data import Dataset as TorchDataset
 
 
@@ -8,31 +12,33 @@ class TorchTabularTextDataset(TorchDataset):
     :obj:`TorchDataset` wrapper for text dataset with categorical features
     and numerical features
 
-    Parameters:
-        encodings (:class:`transformers.BatchEncoding`):
-            The output from encode_plus() and batch_encode() methods (tokens, attention_masks, etc) of
-            a transformers.PreTrainedTokenizer
-        categorical_feats (:class:`numpy.ndarray`, of shape :obj:`(n_examples, categorical feat dim)`, `optional`, defaults to :obj:`None`):
-            An array containing the preprocessed categorical features
-        numerical_feats (:class:`numpy.ndarray`, of shape :obj:`(n_examples, numerical feat dim)`, `optional`, defaults to :obj:`None`):
-            An array containing the preprocessed numerical features
-        labels (:class: list` or `numpy.ndarray`, `optional`, defaults to :obj:`None`):
-            The labels of the training examples
-        df (:class:`pandas.DataFrame`, `optional`, defaults to :obj:`None`):
-            Model configuration class with all the parameters of the model.
-            This object must also have a tabular_config member variable that is a
-            TabularConfig instance specifying the configs for TabularFeatCombiner
+    :param encodings:
+        The output from `encode_plus()` and `batch_encode()` methods (tokens, attention_masks, etc.) of a `transformers.PreTrainedTokenizer`.
 
+    :param categorical_feats:
+        An array containing the preprocessed categorical features. Shape: `(n_examples, categorical feat dim)`.
+
+    :param numerical_feats:
+        An array containing the preprocessed numerical features. Shape: `(n_examples, numerical feat dim)`.
+
+    :param labels:
+        The labels of the training examples.
+
+    :param df:
+        The original dataset. Optional and used only to save the original dataset with the preprocessed dataset.
+
+    :param label_list:
+        A list of class names for each unique class in labels.
     """
 
     def __init__(
         self,
-        encodings,
-        categorical_feats,
-        numerical_feats,
-        labels=None,
-        df=None,
-        label_list=None,
+        encodings: transformers.BatchEncoding,
+        categorical_feats: Optional[pd.DataFrame],
+        numerical_feats: Optional[pd.DataFrame],
+        labels: Optional[Union[List, np.ndarray]] = None,
+        df: Optional[pd.DataFrame] = None,
+        label_list: Optional[List[Union[str]]] = None,
     ):
         self.df = df
         self.encodings = encodings
@@ -45,7 +51,7 @@ class TorchTabularTextDataset(TorchDataset):
             else [i for i in range(len(np.unique(labels)))]
         )
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
         item["labels"] = (
             torch.tensor(self.labels[idx]) if self.labels is not None else None
@@ -62,9 +68,9 @@ class TorchTabularTextDataset(TorchDataset):
         )
         return item
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.encodings["input_ids"])
 
-    def get_labels(self):
-        """returns the label names for classification"""
+    def get_labels(self) -> Optional[List[Union[str]]]:
+        """Returns the label names for classification."""
         return self.label_list
