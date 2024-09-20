96ae7836126e6acf79e5eb912d3aed4d10b723e2
Akash Saravanan <akash.saravanan@georgian.io>
2024-09-20 10:51:52 -0600

    docs: update Sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath("../../"))
 print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
-project = 'Multimodal Transformers'
-copyright = '2020, Ken Gu'
-author = 'Ken Gu'
+project = "Multimodal Transformers"
+copyright = "2020, Ken Gu"
+author = "Ken Gu"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,52 +29,52 @@ author = 'Ken Gu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax',
-    'recommonmark',
-    'sphinx_markdown_tables',
-    'sphinx.ext.githubpages'
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "recommonmark",
+    "sphinx_markdown_tables",
+    "sphinx.ext.githubpages",
+    "sphinx_autodoc_typehints"
 ]
-    # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
 autosummary_generate = True
 
-source_suffix = ['.rst', '.md']
-master_doc = 'index'
+source_suffix = [".rst", ".md"]
+master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+napoleon_use_param = True 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
+html_static_path = []
 add_module_names = False
-
 
 def setup(app):
     def skip(app, what, name, obj, skip, options):
         members = [
-            '__init__',
-            '__repr__',
-            '__weakref__',
-            '__dict__',
-            '__module__',
+            "__init__",
+            "__repr__",
+            "__weakref__",
+            "__dict__",
+            "__module__",
         ]
         return True if name in members else skip
 
-    app.connect('autodoc-skip-member', skip)
+    app.connect("autodoc-skip-member", skip)
